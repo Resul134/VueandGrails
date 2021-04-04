9e90062c58b531cdf49e5a52e3b96fbec1246413
Resul134 <Resulkurrr134@gmail.com>
2021-04-04 14:22:53 +0200

    Kompononter, interface og refakturering.

---
+++ vueandgrails/client/src/components/form/FieldSelect.vue
@@ -0,0 +1,40 @@
+<template id="driverSelect-template" xmlns="http://www.w3.org/1999/xhtml">
+  <div class="form-group"> 
+    <select class="form-control" v-model="selected"> 
+      <option disabled :value="null">Select a {{field}}</option> 
+      
+      <option v-for="value in values" :value="value.id" :key="value.id">
+        {{ value.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  props: {
+      values: Array,
+      field: String
+  }, 
+  data: function () { 
+    return {
+      selected: null 
+    }
+  },
+//   methods: { 
+//     updateValue: function () { 
+//       this.$emit('input', this.selected)
+//     }
+//   },
+  watch: {
+      selected(newValue, oldValue){
+          this.$emit('input', this.selected)
+          console.log(`Gamle værdi ${oldValue} - Ny værdi ${newValue}`)
+      } //refaktuering. Bedre at benytte watch end @change.
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vueandgrails/client/src/types/vehicle.ts
@@ -0,0 +1,8 @@
+export default class Vehicle {
+    id: Number
+    name: String
+    make: {
+        name: string
+        id: Number
+    }
+}
